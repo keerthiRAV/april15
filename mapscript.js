975de52dc9d40812b7d18272c265fd1b36438011
keerthiRAV <keerthiraveendran93@gmail.com>
2019-04-15 15:13:37 -0400

    fourth commit

--- mapscript.js
+++ mapscript.js
@@ -12,9 +12,9 @@ function initialize(){
         zoom:18,
         center: {lat: 40.758904,lng:-73.985128}
     };
-    map1: new google.maps.Map(document.getElementById(map_canvas1),mapOptions1);
-    map2: new google.maps.Map(document.getElementById(map_canvas2),mapOptions2);
-    map3: new google.maps.Map(document.getElementById(map_canvas3),mapOptions3);
+    map1: new google.maps.Map(document.getElementById("map_canvas1"),mapOptions1);
+    map2: new google.maps.Map(document.getElementById("map_canvas2"),mapOptions2);
+    map3: new google.maps.Map(document.getElementById("map_canvas3"),mapOptions3);
 
 }
 google.maps.event.addDomListener(window,'load',initialize);
